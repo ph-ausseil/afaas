e5d40138a872e3ddcd43bf8ab821af80c541aa62
ph-ausseil <ph.ausseil@gmail.com>
2023-07-16 13:01:35 +0200

    INIT main.py

--- autogpt/core/runner/cli_app/main.py
+++ autogpt/core/runner/cli_app/main.py
@@ -1,53 +1,26 @@
 import click
 
-from autogpt.core.agent import AgentSettings, SimpleAgent
-from autogpt.core.runner.client_lib.logging import get_client_logger
+from autogpt.core.agent import AgentSettings, SimpleAgent 
+from autogpt.core.runner.cli_web_app.server.services.agent import workspace_loader , get_logger_and_workspace
 
 
 async def run_auto_gpt(user_configuration: dict):
-    """Run the Auto-GPT CLI client."""
+    
+    # Get the logger and the workspace movec to a function
+    # Because almost every API end-point will excecute this piece of code
+    client_logger, agent_workspace = get_logger_and_workspace(user_configuration)
 
-    client_logger = get_client_logger()
-    client_logger.debug("Getting agent settings")
 
-    agent_workspace = (
-        user_configuration.get("workspace", {}).get("configuration", {}).get("root", "")
-    )
-
-    if not agent_workspace:  # We don't have an agent yet.
-        #################
-        # Bootstrapping #
-        #################
-        # Step 1. Collate the user's settings with the default system settings.
-        agent_settings: AgentSettings = SimpleAgent.compile_settings(
-            client_logger,
-            user_configuration,
-        )
-
-        # Step 2. Get a name and goals for the agent.
-        # First we need to figure out what the user wants to do with the agent.
-        # We'll do this by asking the user for a prompt.
-        user_objective = click.prompt("What do you want Auto-GPT to do?")
-        # Ask a language model to determine a name and goals for a suitable agent.
-        name_and_goals = await SimpleAgent.determine_agent_name_and_goals(
-            user_objective,
-            agent_settings,
-            client_logger,
-        )
-        print(parse_agent_name_and_goals(name_and_goals))
-        # Finally, update the agent settings with the name and goals.
-        agent_settings.update_agent_name_and_goals(name_and_goals)
-
-        # Step 3. Provision the agent.
-        agent_workspace = SimpleAgent.provision_agent(agent_settings, client_logger)
-        print("agent is provisioned")
+    # Get the logger and the workspace moved to a function
+    # Because API end-point will treat this as an error & break
+    if not agent_workspace : 
+        agent_workspace = await workspace_loader(user_configuration, client_logger, agent_workspace)
 
     # launch agent interaction loop
     agent = SimpleAgent.from_workspace(
         agent_workspace,
         client_logger,
     )
-    print("agent is loaded")
 
     plan = await agent.build_initial_plan()
     print(parse_agent_plan(plan))
@@ -61,15 +34,6 @@ async def run_auto_gpt(user_configuration: dict):
         )
         ability_result = await agent.execute_next_ability(user_input)
         print(parse_ability_result(ability_result))
-
-
-def parse_agent_name_and_goals(name_and_goals: dict) -> str:
-    parsed_response = f"Agent Name: {name_and_goals['agent_name']}\n"
-    parsed_response += f"Agent Role: {name_and_goals['agent_role']}\n"
-    parsed_response += "Agent Goals:\n"
-    for i, goal in enumerate(name_and_goals["agent_goals"]):
-        parsed_response += f"{i+1}. {goal}\n"
-    return parsed_response
 
 
 def parse_agent_plan(plan: dict) -> str:
