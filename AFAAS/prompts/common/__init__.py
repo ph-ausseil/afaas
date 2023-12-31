f230084dd0a2579afc375f3a1cdc57d6da28c09b
ph-ausseil <ph.ausseil@gmail.com>
2023-12-31 17:54:07 +0100

    Update __init__.py
    Signed-off-by: ph-ausseil <ph.ausseil@gmail.com>

--- AFAAS/prompts/common/__init__.py
+++ AFAAS/prompts/common/__init__.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
-<<<<<<< HEAD
-=======
+
 
 from .afaas_smart_rag import *
 from .afaas_task_default_summary import *
@@ -10,5 +9,3 @@ from .afaas_task_rag_step3_related import *
 from .autocorrection import *
 
 
-
->>>>>>> parent of 2d284c8d (Apply autoflake fixes)
