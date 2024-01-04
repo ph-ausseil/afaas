a2b378ff18adb164dcf51d3bfc571538280e404c
ph-ausseil <ph.ausseil@gmail.com>
2024-01-05 00:53:45 +0100

    INIT query_language_model tool (strategy remain to do)

--- AFAAS/core/tools/builtins/query_language_model.py
+++ AFAAS/core/tools/builtins/query_language_model.py
@@ -19,7 +19,7 @@ LOG = AFAASLogger(name=__name__)
 @tool(
     name="query_language_model",
     description=(
-        "Search the web and with the capacity of returning steerable & structured result. Not very good at retriving data published over the last 2 years."
+        "Search the web and with the capacity of returning steerable & structured result. Not very good at retriving data published last month."
     ),
     parameters={
         "query": JSONSchema(
@@ -30,18 +30,23 @@ LOG = AFAASLogger(name=__name__)
         "format": JSONSchema(
             type=JSONSchema.Type.STRING,
             description="Describe the format (plan, length,...) of the expected answer.",
+            required=True,
         ),
         "answer_as": JSONSchema(
             type=JSONSchema.Type.STRING,
-            description="Describe person with his profession, mindeset, personnality ect... You would like to get an answer from.",
+            description="Describe the ideal persona persona with his profession, expertise, mindset, personnality, ect... You would like to get an answer from.",
+            required=True,
+        ),
+        "example": JSONSchema(
+            type=JSONSchema.Type.STRING,
+            description="As large language model, this tool will give much  better results if given example of phrasing, format, ext you may expept.",
         ),
     },
 )
 async def query_language_model(task: Task, agent: BaseAgent) -> None:
-    # plan =  self.execute_strategy(
-    agent._loop.tool_registry().list_tools_descriptions()
-    plan = await agent._loop._execute_strategy(
-        strategy_name="make_initial_plan", agent=agent
+
+    plan = await agent.execute_strategy(
+        strategy_name="query_llm", agent=agent, task=task
     )
 
     return plan
