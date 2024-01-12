e8375ed89881cc82a22d352f515150613c3643a8
ph-ausseil <ph.ausseil@gmail.com>
2024-01-12 16:54:18 +0100

    Fix a bug in the demo disturbing the creation of new Agent (#127)

--- app/cli_app/main.py
+++ app/cli_app/main.py
@@ -50,6 +50,7 @@ async def run_cli_demo():
         user_id=user_id,
     )
 
+    selected_agent_index: int = 0
     if len(agent_dict_list) > 0:
         LOG.info(f"User {user_id} has {len(agent_dict_list)} agents.")
         if CONSOLE_LOG_LEVEL > logging.DEBUG:
@@ -59,7 +60,7 @@ async def run_cli_demo():
                     f"{i+1}. {agent_dict['agent_name']}({agent_dict['agent_id']}) : {agent_dict['agent_goal_sentence']}"
                 )
 
-            selected_agent_index: int = 0
+
             i: int = 0
             while selected_agent_index < 1 or selected_agent_index > len(
                 agent_dict_list
@@ -81,6 +82,8 @@ async def run_cli_demo():
         else:
             selected_agent_index: int = 1
 
+
+    if (selected_agent_index > 0)  :     
         agent_dict = agent_dict_list[selected_agent_index - 1]
         agent_settings = PlannerAgent.SystemSettings(
             **agent_dict_list[selected_agent_index - 1]
@@ -91,7 +94,6 @@ async def run_cli_demo():
         agent: PlannerAgent = PlannerAgent.get_instance_from_settings(
             agent_settings=agent_settings
         )
-
     else:
         #
         # New requirement gathering process
@@ -111,9 +113,7 @@ async def run_cli_demo():
         agent_settings._type_ = "AFAAS.core.agents.planner.main.PlannerAgent"
 
         # Step 3. Create the agent.
-        agent_settings_dict = agent_settings.dict()
-        agent_settings_dict["settings"] = agent_settings
-        agent: PlannerAgent = PlannerAgent(**agent_settings_dict)
+        agent: PlannerAgent = await PlannerAgent.load(settings = agent_settings , **agent_settings.dict())
 
     await agent.run(
         user_input_handler=handle_user_input_request,
